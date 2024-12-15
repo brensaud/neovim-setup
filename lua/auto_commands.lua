64e8fd6187eda900319b5a969ebd495fe85b5540
Bren Saud <Bren.Saud@verisk.com>
2024-12-15 18:04:55 +0545

    add auto command to detech whether the project is python project or file is python file so that we can select proper python environment

--- lua/auto_commands.lua
+++ lua/auto_commands.lua
@@ -1,8 +1,16 @@
 -- Auto commands
+
+
+-- Set filetype to Python for .py files
+
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
   pattern = "*.py",
   command = "set filetype=python",
 })
+
+
+
+-- Check if Treesitter parser is available for the filetype
 
 vim.api.nvim_create_autocmd('FileType', {
   callback = function()
@@ -15,6 +23,8 @@ vim.api.nvim_create_autocmd('FileType', {
     end
   end
 })
+
+
 
 -- Autocmd to toggle relative numbers in normal mode and absolute numbers in insert mode
 vim.api.nvim_create_augroup("ToggleRelativeNumber", { clear = true })
@@ -32,5 +42,19 @@ vim.api.nvim_create_autocmd({ "InsertLeave" }, {
   callback = function()
     vim.opt.relativenumber = true  -- Enable relative numbers in normal mode
     vim.opt.number = true          -- Keep absolute numbers
+  end,
+})
+
+
+-- Autocommand to enable Python provider dynamically for Python files or projects
+local set_python_host_prog = require("python_provider").set_python_host_prog
+
+vim.api.nvim_create_autocmd({ "FileType", "BufEnter" }, {
+  callback = function()
+    local filetype = vim.bo.filetype
+    local cwd = vim.fn.getcwd()
+    if filetype == "python" or vim.fn.isdirectory(cwd .. "/.venv") == 1 or vim.loop.fs_stat(cwd .. "/requirements.txt") then
+      set_python_host_prog()
+    end
   end,
 })
