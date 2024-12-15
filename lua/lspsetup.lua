e31e1a835a1cc7631e6a6f8777537801ef04eee8
Bren Saud <Bren.Saud@verisk.com>
2024-12-15 17:31:38 +0545

    add lsp setup

---
+++ lua/lspsetup.lua
@@ -0,0 +1,31 @@
+
+-- LSP Setup (Lua Language Server Fix)
+local lspconfig = require("lspconfig")
+require("mason-lspconfig").setup_handlers({
+  function(server_name)
+    lspconfig[server_name].setup({
+      capabilities = require("cmp_nvim_lsp").default_capabilities(),
+    })
+  end,
+})
+
+-- Lua Language Server for Neovim
+lspconfig.lua_ls.setup({
+  settings = {
+    Lua = {
+      runtime = {
+        version = "LuaJIT",
+      },
+      diagnostics = {
+        globals = { "vim" },
+      },
+      workspace = {
+        library = vim.api.nvim_get_runtime_file("", true),
+        checkThirdParty = false,
+      },
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+})
